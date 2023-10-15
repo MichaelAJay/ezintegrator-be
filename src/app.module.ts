a353e7ab593c6da30fcb73abc1190d7026a4af96
mjay <michael@merged.group>
2023-10-15 14:36:40 -0400

    removed boilerplate app controller & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [
     ThrottlerModule.forRoot([{ ttl: 60, limit: 100 }]),
     ConfigModule.forRoot(),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
