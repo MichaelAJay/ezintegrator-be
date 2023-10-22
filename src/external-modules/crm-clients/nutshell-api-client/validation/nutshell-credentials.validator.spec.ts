3fcdb87e2b3688dd19a143a8b7167ccdc4387faa
mjay <michael@merged.group>
2023-10-22 16:31:06 -0400

    wrote nutshell credentials validator tests

---
+++ src/external-modules/crm-clients/nutshell-api-client/validation/nutshell-credentials.validator.spec.ts
@@ -0,0 +1,25 @@
+import { INutshellCredentials } from '../interfaces';
+import { validateNutshellCredentials } from './nutshell-credentials.validator';
+
+describe('validateNutshellCredentials unit tests', () => {
+  describe('valid data', () => {
+    it('returns true', async () => {
+      const validCredentials: INutshellCredentials = {
+        username: 'USERNAME EXISTS',
+        apiKeySecretName: 'API KEY SECRET NAME EXISTS',
+      };
+
+      expect(validateNutshellCredentials(validCredentials)).toBe(true);
+    });
+  });
+  describe('invalid data', () => {
+    it('throws error with message "Data failed validation"', async () => {
+      const invalidResponse = {
+        userName: 'NOTE CASING',
+      };
+      expect(() => {
+        validateNutshellCredentials(invalidResponse);
+      }).toThrow('Data failed validation');
+    });
+  });
+});
--- src/external-modules/crm-clients/nutshell-api-client/validation/nutshell-credentials.validator.ts
+++ src/external-modules/crm-clients/nutshell-api-client/validation/nutshell-credentials.validator.ts
@@ -1,6 +1,6 @@
 import { JSONSchemaType } from 'ajv';
-import { validateWithAjv } from 'src/utility';
-import { ajv } from 'src/utility/singletons/ajv.singleton';
+import { validateWithAjv } from '../../../../utility';
+import { ajv } from '../../../../utility/singletons/ajv.singleton';
 import { INutshellCredentials } from '../interfaces';
 
 const nutshellCredentialsSchema: JSONSchemaType<INutshellCredentials> = {
