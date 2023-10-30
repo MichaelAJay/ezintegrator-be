c4c237bd84ae924ebbe5d0fbcb04e16e3fadafad
mjay <michael@merged.group>
2023-10-29 20:53:12 -0400

    Implemented crypto handler - generateSalt, hash, validateHash

--- src/internal-modules/security-utility/interfaces/class-interfaces/crypto-utility.class-interface.ts
+++ src/internal-modules/security-utility/interfaces/class-interfaces/crypto-utility.class-interface.ts
@@ -1,1 +1,9 @@
-export interface ICryptoUtility {}
+export interface ICryptoUtility {
+  generateSalt(): string;
+  hash(value: string, salt: string): Promise<string>;
+  validateSaltedHash(
+    unhashedValue: string,
+    hashedValue: string,
+    salt: string,
+  ): Promise<boolean>;
+}
