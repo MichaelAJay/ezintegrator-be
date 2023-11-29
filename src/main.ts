5f8740563ab5c9c86558fdebd315e70a40f63e13
mjay <michael@merged.group>
2023-11-28 22:46:45 -0500

    updated with cors

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,35 @@ import { AppModule } from './app.module';
 import fastifyCookie from '@fastify/cookie';
 import { getEnvironmentVariable } from './utility';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {
+  CorsOptions,
+  CorsOptionsDelegate,
+} from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
+  const FRONTEND_BASE_URL = getEnvironmentVariable('FRONTEND_BASE_URL');
+  const corsOrigin = [FRONTEND_BASE_URL];
+
+  const corsOptions: CorsOptions | CorsOptionsDelegate<any> = {
+    origin: corsOrigin,
+    credentials: true,
+    allowedHeaders: [
+      'Access-Control-Allow-Origin',
+      'Origin',
+      'X-Requested-With',
+      'Accept',
+      'Content-Type',
+      'Authorization',
+    ],
+    exposedHeaders: 'Authorization',
+    methods: ['GET', 'PUT', 'OPTIONS', 'POST', 'DELETE', 'PATCH'],
+  };
+
+  const adapter = new FastifyAdapter();
+  adapter.enableCors(corsOptions);
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    adapter,
   );
 
   await app.register(fastifyCookie, {
